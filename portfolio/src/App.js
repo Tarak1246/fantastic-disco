4438012ec6026a2d096120800f922ca4ac4ebe03
Tarak1246 <tarak1246@gmail.com>
2024-07-08 15:19:33 -0400

    initial code structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Tarak's portfolio...in progress
+    </div>
+  );
+}
+
+export default App;
